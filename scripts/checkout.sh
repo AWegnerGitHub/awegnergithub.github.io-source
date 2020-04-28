f84faa5427901845df8360b4cc607993b99ae266
Andy Wegner <andy@andrewwegner.com>
2020-04-28 10:29:17 -0500

    Adjusting some scripts

--- scripts/checkout.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/AWegnerGithub/awegnergithub.github.io-source.git source
-mkdir source/content
-rsync -av --progress ./* source/content --exclude source
-cd source
-pip install --upgrade pip
-pip install -r requirements.txt
--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,5 +1,5 @@
 pip install pylinkvalidator
 python -m pelican.server && cd - &
 sleep 3
-linkchecker http://localhost:8000
+pylinkvalidate.py -P http://localhost:8000
 pgrep -f "^python -m pelican.server" | xargs kill -9
